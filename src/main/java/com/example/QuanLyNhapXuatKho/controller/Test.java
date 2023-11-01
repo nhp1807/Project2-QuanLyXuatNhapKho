be038e74edd2fbc3991cef9a63315bd75f86c33c
Phong <nhp1807@gmail.com>
2023-11-01 21:51:16 +0700

    Add some test

--- src/main/java/com/example/QuanLyNhapXuatKho/controller/Test.java
+++ src/main/java/com/example/QuanLyNhapXuatKho/controller/Test.java
@@ -2,9 +2,12 @@ package com.example.QuanLyNhapXuatKho.controller;
 
 public class Test {
     public static void main(String[] args) {
-        String str = "AD123";
-        int currentMa = Integer.parseInt(str.substring(2));
+        float number = 0.0f;
 
-        System.out.println(currentMa);
+        for(int i = 0; i < 100; i++){
+            number += 0.2f;
+        }
+
+        System.out.println("\n" + number + "\n");
     }
 }
